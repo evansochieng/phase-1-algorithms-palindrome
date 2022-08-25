a30c8cce3a26d8c306097354525c4faad1006de4
Evans Ochieng <evansochieng121@gmail.com>
2022-08-25 13:50:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // Reverse the letters of the word and create a new
+  const reversedWord = word.split('').reverse().join('');
+
+  // Check if reversed word and original word match and return
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  declare a function that takes a word as argument
+      split the word to its letters, reverse the resulting array and join the letters
+      store the result in a variable
+
+      compare the reversed word and the input argument
+      return the output of the comparison
 */
 
 /*
   Add written explanation of your solution here
+  I want my function to take a string argument, then split this at every single letter.
+  This will return an array of letters which I then reverse.
+  There after I join the letters to come up with a reversed string.
+  I compare the reversed string with the input string and return the results
 */
 
 // You can run `node index.js` to view these console logs
